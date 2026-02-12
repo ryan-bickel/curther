55c978bfd5cf606f9a195283187cf56024c9f6a9
Ryan Bickel <rbickel2000@gmail.com>
2026-02-12 16:28:22 -0600

    initial theramin implementation

---
+++ src/theramin.rs
@@ -0,0 +1,47 @@
+use std::sync::Arc;
+use std::sync::atomic::Ordering;
+use std::thread;
+use std::time::Duration;
+use atomic_float::AtomicF32;
+use rodio::{OutputStreamBuilder, SampleRate, Sink, Source};
+use rodio::source::{Function};
+use crate::mutable_signal_generator::MutableSignalGenerator;
+
+const SAMPLE_RATE: SampleRate = 48_000;
+
+struct Theramin {
+    sink: Sink,
+    frequency: Arc<AtomicF32>,
+    amplitude: Arc<AtomicF32>
+}
+
+impl Theramin {
+    pub fn new(frequency: f32, amplitude: f32, function: Function) -> Self {
+        let output_stream = OutputStreamBuilder::open_default_stream()
+            .expect("unable to create output stream");
+        let sink = Sink::connect_new(&output_stream.mixer());
+
+        let frequency_ref = Arc::new(AtomicF32::new(frequency));
+        let frequency_ref_clone = Arc::clone(&frequency_ref);
+
+        let amplitude_ref = Arc::new(AtomicF32::new(amplitude));
+        let amplitude_ref_clone = Arc::clone(&amplitude_ref);
+
+        let source = MutableSignalGenerator::new(SAMPLE_RATE, frequency, amplitude, function)
+            .periodic_access(Duration::from_millis(1000 / 60), move |src| {
+                src.set_frequency(frequency_ref_clone.load(Ordering::Relaxed));
+                src.set_amplitude(amplitude_ref_clone.load(Ordering::Relaxed))
+            });
+        sink.append(source);
+
+        Theramin {
+            sink,
+            frequency: frequency_ref,
+            amplitude: amplitude_ref,
+        }
+    }
+
+    pub fn play(&self) {
+        self.sink.sleep_until_end();
+    }
+}
