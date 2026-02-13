2e32acdf1f08f29abf06dc25d544275e4668c3ff
Ryan Bickel <rbickel2000@gmail.com>
2026-02-12 19:11:25 -0600

    added function command line arg

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,26 @@ mod theremin;
 mod signals;
 mod mutable_signal_generator;
 
+use std::env;
 use rodio::source::Function;
 use crate::mtheremin::MTheremin;
 
 fn main() {
-    let mut mthere = MTheremin::new(880.0, 0.25, Function::Sawtooth);
+    let args: Vec<String> = env::args().collect();
+
+    let function = if (args.len() < 3) {
+        Function::Square
+    } else {
+        let function_string = args[2].as_str();
+        match function_string {
+            "square" => Function::Square,
+            "sawtooth" => Function::Sawtooth,
+            "sine" => Function::Sine,
+            "triangle" => Function::Triangle,
+            _ => Function::Square
+        }
+    };
+
+    let mut mthere = MTheremin::new(2400.0, 0.25, function);
     mthere.play();
 }
